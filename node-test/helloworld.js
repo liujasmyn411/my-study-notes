819ac4cda41e309634e6647de1dff12361f6442b
liujasmyn411 <liujasmyn@163.com>
2025-12-12 15:35:21 +0800

    js

--- node-test/helloworld.js
+++ node-test/helloworld.js
@@ -1,1 +1,5 @@
-console.log("Hello World"); 
+// hello.js
+console.log('Hello, Node.js!');
+console.log('Node.js 版本：', process.version);
+console.log('当前工作目录：', process.cwd());
+console.log('操作系统：', process.platform);
